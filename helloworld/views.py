67455a1dbf8f57583aef8bb5bc98b907343a1431
proud609 <43259240+proud609@users.noreply.github.com>
2018-10-04 10:56:39 +0800

    'new'

--- helloworld/views.py
+++ helloworld/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,redirect   # 加入 redirect 套件
 from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
+from datetime import datetime
 from django.contrib.auth.models import User
 
-
+ 
 def index(request):
-	return render(request, 'jackproj.html')
+	ClassList=map (str , range(100))
+	return render(request, 'jackproj.html',{'ClassList':ClassList})
